95168a1965c74d5772afc615fb77caf20be82602
Yehor Popovych <yehor@crossroad.io>
2019-12-11 17:39:40 +0200

    updated popup

--- packages/openwallet/src/error.ts
+++ packages/openwallet/src/error.ts
@@ -8,36 +8,74 @@ const popupStyle = `
   <style id="${popupStyleId}" type="text/css">
     #${popupId} {
       position: fixed;
-      z-index: 2000;
+      z-index: 9999;
       top: 0;
       right: 0;
       bottom: 0;
       left: 0;
       display: block;
-      background-color: rgba(11, 15, 44, 0.64);
+      background-color: rgba(0, 0, 0, 0.64);
       overflow: auto;
       animation: ${popupId}-fadeIn .3s ease;
     }
+    #${popupId}.hide {
+      animation: ${popupId}-fadeOut .3s ease;
+    }
     #${popupId} .container {
       position: fixed;
-      padding: 16px;
       z-index: 999;
       top: 50%;
       left: 50%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border-radius: 24px;
-      background-color: #fff;
-      transform: translate(-50%,-50%);
-      overflow: hidden;
-    }
-    #${popupId} .content {
-      position: relative;
+      width: 85vw;
+      max-width: 400px;
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
+      transform: translate(-50%,-50%);
+      overflow: hidden;
+    }
+    #${popupId} .content {
+      padding: 40px 24px;
+      position: relative;
+      border-radius: 24px;
+      width: 100%;
+      background-color: #fff;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+    #${popupId} .title {
+      line-height: 28px;
+      font-size: 24px;
+      text-align: center;
+    }
+    #${popupId} .description {
+      margin-top: 16px;
+      padding: 0;
+      line-height: 24px;
+      font-size: 16px;
+      text-align: center;
+      opacity: .64;
+    }
+    #${popupId} .store-button-ios {
+      overflow:hidden;
+      margin-top: 24px;
+      width:135px;
+      height:40px;
+      cursor: pointer;
+    }
+    #${popupId} .close-button {
+      margin-top: 24px;
+      cursor: pointer;
+      width: 100%;
+      height: 50px;
+      border-radius: 12px;
+      background-color: #fff;
+      font-size: 18px;
+      font-weight: 500;
+      color: #000;
     }
     @keyframes ${popupId}-fadeIn {
       0% {
@@ -47,11 +85,19 @@ const popupStyle = `
         opacity: 1;
       }
     }
+    @keyframes ${popupId}-fadeOut {
+      0% {
+        opacity: 1;
+      }
+      100% {
+        opacity: 0;
+      }
+    }
   </style>
 `
 
 const storeUrls = {
-  iOS: "https://apps.apple.com/us/app/id1459505103",
+  iOS: "https://apps.apple.com/us/app/tesseract-wallet/id1459505103?mt=8",
   android: null,
   unknown: null
 }
@@ -60,37 +106,40 @@ const popupHtml = `
   <div id="${popupId}">
     <div class="container">
       {{body}}
-      <button class="close-button">OK</button>
+      <button class="close-button">Close</button>
     </div>
   </div>
 `
 
 const iOSHtml = `
   <div class="content">
-    <p>Wallet is not installed.</p>
-    <button class="store-button">
-      Go to AppStore
-    </button>
+    <span class="title">Install Wallet</span>
+    <p class="description">To use this dApp you have to install Tesseract protocol compatible wallet from the AppStore</p>
+    <img
+      src="https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2019-05-01&kind=iossoftware&bubble=ios_apps"
+      class="store-button store-button-ios"
+    ></img>
   </div>
 `
 
 const androidHtml = `
   <div class="content">
-    <p>Wallet is not installed.</p>
-    <p>Android is not supported yet.</p>
-    <p>We are working on it!</p>
+    <span class="title">Install Wallet</span>
+    <p class="description">Android is not supported yet.<br>We are working on it!</p>
   </div>
 `
 
 const unknowPlatformHtml = `
   <div class="content">
-    <p>Wallet is not installed.</p>
-    <p>Platform is not supported</p>
+    <span class="title">Install Wallet</span>
+    <p class="description">Sorry, your platform is not supported yet.</p>
   </div>
 `
 
 function close() {
-  document.getElementById(popupId)!.remove();
+  const popup = document.getElementById(popupId)!
+  popup.addEventListener('animationend', function () { this.remove() })
+  popup.classList.add('hide')
 }
 
 function openUrl(url: string) {
